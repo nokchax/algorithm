3a3e6372c5eddaa6b1857bf9d107a6e59b552f42
nokchax <nokchax@gmail.com>
2019-05-19 20:38:45 +0900

    190519

---
+++ src/Q00094/Main.java
@@ -0,0 +1,4 @@
+package Q00094;
+
+public class Main {
+}
---
+++ src/Q00094/Solution.java
@@ -0,0 +1,29 @@
+package Q00094;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+
+        if(root != null)
+            inorder(root, result);
+
+
+        return result;
+    }
+
+    public List<Integer> inorder(TreeNode node, List<Integer> result) {
+        if(node == null)
+            return  result;
+
+        inorder(node.left, result);
+        result.add(node.val);
+        inorder(node.right, result);
+
+        return result;
+    }
+}
+
