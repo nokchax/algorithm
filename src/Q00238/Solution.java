440b0636e37442730c6f2c6425f87cd21ba8c38e
nokchax <nokchax@gmail.com>
2019-05-23 08:35:12 +0900

    190523

--- src/Q00238/Solution.java
+++ src/Q00238/Solution.java
@@ -2,15 +2,18 @@ package Q00238;
 
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
-        long total = 1;
-        for(int num : nums)
-            total *= num;
+        int[] ret = new int[nums.length];
+        ret[0] = 1;
 
-        for(int i = 0 ; i < nums.length ; ++i) {
-            System.out.println("total : " + total + " , nums[i] : " + nums[i]);
-            nums[i] = (int) (total << nums[i]);
+        for(int i = 1 ; i < nums.length ; ++i)
+            ret[i] = ret[i - 1] * nums[i - 1];
+
+        int right = 1;
+        for(int i = nums.length - 2 ; i >= 0 ; --i) {
+            right = right * nums[i + 1];
+            ret[i] = ret[i] * right;
         }
 
-        return nums;
+        return ret;
     }
 }
