5a09c79e7427ad939d4c392f1b42128e3e305e7b
nokchax <nokchax@gmail.com>
2019-05-24 16:22:33 +0900

    190524

---
+++ src/Q00022/Solution.java
@@ -0,0 +1,40 @@
+package Q00022;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    List<String> answer = new ArrayList<>();
+    private static int limit = 0;
+    private static int total = 0;
+    private static int opener = 0;
+    private static int closer = 0;
+
+    public List<String> generateParenthesis(int n) {
+        limit = n;
+        total = 2*n;
+
+        dfs("");
+
+        return answer;
+    }
+
+    public void dfs(String parenthesis) {
+        if(parenthesis.length() == total) {
+            answer.add(parenthesis);
+            return;
+        }
+
+        if(opener < limit) {
+            opener++;
+            dfs(parenthesis + "(");
+            opener--;
+        }
+        if(closer < opener) {
+            closer++;
+            dfs(parenthesis + ")");
+            closer--;
+        }
+
+    }
+}
