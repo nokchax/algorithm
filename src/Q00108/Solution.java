951ba25c38fedc21e8c43a1cc5654aab8170d270
nokchax <nokchax@gmail.com>
2019-05-28 16:22:22 +0900

    add comment

--- src/Q00108/Solution.java
+++ src/Q00108/Solution.java
@@ -10,7 +10,7 @@ public class Solution {
     public TreeNode makeTreeNode(int[] arr, int left, int right) {
         if(left > right)
             return null;
-        int mid = (left + right + 1) / 2;
+        int mid = (left + right + 1) / 2; // (left + right) / 2 also accepted
 
         TreeNode treeNode = new TreeNode(arr[mid]);
         treeNode.left = makeTreeNode(arr, left, mid - 1);
