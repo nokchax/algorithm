5edc71e8e9c29a30d66c6e832df993e4bf802bf7
nokchax <nokchax@gmail.com>
2019-05-30 08:37:56 +0900

    190530

---
+++ src/Q00070/Solution.java
@@ -0,0 +1,16 @@
+package Q00070;
+
+public class Solution {
+    public int climbStairs(int n) {
+        int[] cache = new int[n + 1];
+
+        cache[1] = 1;
+        if(n > 1)
+            cache[2] = 2;
+
+        for(int i = 3 ; i <= n ; ++i)
+            cache[i] = cache[i - 1] + cache[i - 2];
+
+        return cache[n];
+    }
+}
