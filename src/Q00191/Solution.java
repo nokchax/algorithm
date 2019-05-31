78f749453cf07fd4d0aa5ed2ea13d6ab92b01093
nokchax <nokchax@gmail.com>
2019-05-31 13:53:53 +0900

    190531

---
+++ src/Q00191/Solution.java
@@ -0,0 +1,24 @@
+package Q00191;
+
+public class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+
+        for(char c : Integer.toBinaryString(n).toCharArray()) {
+            if(c == '1')
+                count++;
+        }
+
+        return count;
+    }
+}
+/*
+public static int hammingWeight(int n) {
+	int ones = 0;
+    while(n!=0) {
+    	ones = ones + (n & 1);
+    	n = n>>>1;
+    }
+    return ones;
+}
+ */
