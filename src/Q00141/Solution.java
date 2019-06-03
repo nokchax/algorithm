c6bb96922f8c2ea63911d21d92a598c5346e9469
nokchax <nokchax@gmail.com>
2019-06-04 08:19:53 +0900

    190604

---
+++ src/Q00141/Solution.java
@@ -0,0 +1,24 @@
+package Q00141;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null)
+            return false;
+
+        Set<ListNode> set = new HashSet<>();
+
+        ListNode cur = head;
+        while(cur.next != null) {
+            if(set.contains(cur.next))
+                return true;
+
+            set.add(cur.next);
+            cur = cur.next;
+        }
+
+        return false;
+    }
+}
