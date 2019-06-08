1d2179f78ac75afd838908a3b7ffeb21c00a945c
nokchax <nokchax@gmail.com>
2019-06-08 21:43:50 +0900

    190608

---
+++ src/Q00509/Solution.java
@@ -0,0 +1,17 @@
+package Q00509;
+
+public class Solution {
+    public int fib(int N) {
+        int[] cache = new int[N + 1];
+
+        if(N > 0)
+            cache[1] = 1;
+        if(N > 1)
+            cache[2] = 1;
+
+        for(int i = 3 ; i <= N ; ++i)
+            cache[i] = cache[i - 1] + cache[i - 2];
+
+        return cache[N];
+    }
+}
