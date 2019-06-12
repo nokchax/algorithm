aee3342540af99c5ff4788e589a3d48c4f6a9bec
nokchax <nokchax@gmail.com>
2019-06-12 19:09:46 +0900

    190612

---
+++ src/Q00122/Solution.java
@@ -0,0 +1,18 @@
+package Q00122;
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0 ; i < prices.length ; ++i) {
+            if(prices[i] > min) {
+                profit += (prices[i] - min);
+            }
+
+            min = prices[i];
+        }
+
+        return profit;
+    }
+}
