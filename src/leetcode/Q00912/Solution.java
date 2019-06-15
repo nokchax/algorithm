f8f0266bb874468c24eb15592a4eaef372814f2f
nokchax <nokchax@gmail.com>
2019-06-15 21:31:07 +0900

    190615

---
+++ src/leetcode/Q00912/Solution.java
@@ -0,0 +1,10 @@
+package leetcode.Q00912;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
