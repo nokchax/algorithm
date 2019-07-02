8f4043a827553915f3afb649c5da3cfed76a78d7
nokchax <nokchax@gmail.com>
2019-07-02 19:43:41 +0900

    190702

---
+++ src/leetcode/Q00461/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.Q00461;
+
+public class Solution {
+    public int hammingDistance(int x, int y) {
+        int xor = x ^ y;
+        int count = 0;
+
+        while(xor > 0) {
+            if(xor % 2 == 1)
+                count++;
+            
+            xor /= 2;
+        }
+
+
+        return count;
+    }
+}
