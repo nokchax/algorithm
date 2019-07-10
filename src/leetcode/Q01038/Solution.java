47ca015d03b26de0f91628014322677fdf0d574d
nokchax <nokchax@gmail.com>
2019-07-10 09:07:02 +0900

    190710

---
+++ src/leetcode/Q01038/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.Q01038;
+
+public class Solution {
+    public TreeNode bstToGst(TreeNode root) {
+
+        calc(root, 0);
+        return root;
+    }
+
+    private int calc(TreeNode node, int val) {
+        if(node == null)
+            return val;
+
+        node.val = node.val + calc(node.right, val);
+        return calc(node.left, node.val);
+    }
+}
