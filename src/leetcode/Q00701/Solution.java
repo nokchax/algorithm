56d0da4a52f71a4c02d8a171e1e106a7b24296c9
nokchax <nokchax@gmail.com>
2019-07-14 20:22:46 +0900

    190714

---
+++ src/leetcode/Q00701/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.Q00701;
+
+public class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+
+        return insert(root, val, null, false);
+    }
+
+    private TreeNode insert(TreeNode node, int val, TreeNode prev, boolean left) {
+        if(node == null) {
+            TreeNode newNode = new TreeNode(val);
+            if(prev == null)
+                return newNode;
+            else {
+                if(left)
+                    prev.left = newNode;
+                else
+                    prev.right = newNode;
+            }
+            return prev;
+        }
+
+        if(node.val < val)
+            insert(node.right, val, node, false);
+        else
+            insert(node.left, val, node, true);
+
+        return node;
+    }
+
+}
