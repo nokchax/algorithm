612d39265d6de7235608414d5fa174a297bdd977
nokchax <nokchax@gmail.com>
2019-07-23 09:35:01 +0900

    190723

--- src/leetcode/Q00048/Solution.java
+++ src/leetcode/Q00048/Solution.java
@@ -4,13 +4,25 @@ public class Solution {
     public void rotate(int[][] matrix) {
 
         for(int y = 0 ; y < matrix.length / 2 ; ++y) {
-            for(int x = y ; x < matrix.length ; ++x) {
+            for(int x = y ; x < matrix.length - 1 - y ; ++x) {
                 rotateOne(y, x, matrix);
             }
+        }
+
+        for(int y = 0 ; y < matrix.length ; ++y) {
+            for(int x = 0 ; x < matrix.length ; ++x) {
+                System.out.print(matrix[y][x] + " ");
+            }
+            System.out.println();
         }
     }
 
     private void rotateOne(int y, int x, int[][] matrix) {
+        int limit = matrix.length - 1;
         int temp = matrix[y][x];
+        matrix[y][x] = matrix[limit - x][y];
+        matrix[limit - x][y] = matrix[limit - y][limit - x];
+        matrix[limit - y][limit - x] = matrix[x][limit - y];
+        matrix[x][limit - y] = temp;
     }
 }
