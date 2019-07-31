6f4f343754c37ff17cb6a3755e6bd5126ff01ea3
nokchax <nokchax@gmail.com>
2019-07-31 19:52:11 +0900

    190731

--- src/leetcode/Q00011/Solution.java
+++ src/leetcode/Q00011/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
         int maxArea = 0;
         int left = 0;
         int right = height.length - 1;
-        
+
         while (left < right) {
             maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left));
             if (height[left] < height[right])
