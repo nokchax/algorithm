180938ad3e897e353fe67f2068aaf4ac7373270c
nokchax <nokchax@gmail.com>
2019-09-21 16:26:00 +0900

    190921

--- src/leetcode/Q00312/Solution.java
+++ src/leetcode/Q00312/Solution.java
@@ -2,29 +2,44 @@ package leetcode.Q00312;
 
 public class Solution {
     static int[][] cache;
+    static int[] NUMS;
+
     public int maxCoins(int[] nums) {
         cache = new int[nums.length][nums.length];
-
-        int max = Integer.MIN_VALUE;
+        NUMS = nums;
+        int max = 0;
 
         for(int i = 0 ; i < nums.length ; ++i) {
-            max = Math.max(max, calcMax(i, -1, i));
-            int leftMax = calcMax(i, -1, i);
-            int rightMax = calcMax(i, i, nums.length - 1);
-
+            max = Math.max(max,
+                            burst(0, i - 1) +
+                            burst(i + 1, nums.length - 1) +
+                            NUMS[i]
+                    );
         }
 
-        return 1;
+        return max;
     }
 
-    private int calcMax(int pick, int leftLimit, int rightLimit) {
-        if()
+    private int burst(int left, int right) {
+        if(left < 0 || left > right || right >= cache.length)
+            return 0;
 
+        if(cache[left][right] > 0)
+            return cache[left][right];
 
-        return -1;
-    }
+        int max = 0;
 
-    private int getMax(int max) {
-        return 0;
+        for(int i = left ; i <= right ; ++i) {
+            int sum = burst(left, i - 1) + burst(i + 1, right);
+            int leftVal = left - 1 < 0 ? 1 : NUMS[left - 1];
+            int rightVal = right + 1 >= cache.length ? 1 : NUMS[right + 1];
+            sum += NUMS[i] * leftVal * rightVal;
+
+            max = Math.max(max, sum);
+        }
+
+        cache[left][right] = max;
+
+        return max;
     }
 }
---
+++ src/leetcode/Q00312/Test.java
@@ -0,0 +1,11 @@
+package leetcode.Q00312;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+
+        System.out.println(solution.maxCoins(new int[] {3,1,5,8}));
+        System.out.println(solution.maxCoins(new int[] {9,76,64,21,97,60,5}));
+    }
+}
