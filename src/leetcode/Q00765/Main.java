a3b1374df3a7a000c2a13bf3f1b6621c60a9c489
nokchax <nokchax@gmail.com>
2019-10-18 13:44:34 +0900

    time excceded

--- src/leetcode/Q00765/Main.java
+++ src/leetcode/Q00765/Main.java
@@ -1,36 +1,42 @@
 package leetcode.Q00765;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         Solution s = new Solution();
+//        System.out.println(s.minSwapsCouples(new int[] {0,2,1,3}));
+//        System.out.println(s.minSwapsCouples(new int[] {8,5,3,4,1,2,7,6,9,0}));
+        System.out.println(s.minSwapsCouples(new int[] {10,12,20,18,14,11,6,19,16,2,21,15,7,1,4,8,0,17,9,5,13,3}));
 
-        int[] a = new int[] {1, 2, 3, 4};
-        int[] b = new int[] {1, 2, 3, 4};
 
-        System.out.println(a);
-        System.out.println(b);
-
-        System.out.println("===============================");
-
-        System.out.println(Arrays.toString(a));
-        System.out.println(Arrays.toString(b));
-
-        System.out.println("===============================");
-
-        System.out.println(a == b);
-        System.out.println("===============================");
-
-        System.out.println(a.equals(b));
-        System.out.println(Arrays.equals(a, b));
-
-        System.out.println("===============================");
-
-        int temp = a[0];
-        a[0] = a[1];
-        a[1] = temp;
-
-        System.out.println(Arrays.toString(a));
+//        int[] a = new int[] {1, 2, 3, 4};
+//        int[] b = new int[] {1, 2, 3, 4};
+//
+//        System.out.println(a);
+//        System.out.println(b);
+//
+//        System.out.println("===============================");
+//
+//        System.out.println(Arrays.toString(a));
+//        System.out.println(Arrays.toString(b));
+//
+//        System.out.println("===============================");
+//
+//        System.out.println(a == b);
+//        System.out.println("===============================");
+//
+//        System.out.println(a.equals(b));
+//        System.out.println(Arrays.equals(a, b));
+//
+//        System.out.println("===============================");
+//
+//        int temp = a[0];
+//        a[0] = a[1];
+//        a[1] = temp;
+//
+//        System.out.println(Arrays.toString(a));
     }
 }
--- src/leetcode/Q00765/Solution.java
+++ src/leetcode/Q00765/Solution.java
@@ -3,36 +3,36 @@ package leetcode.Q00765;
 import java.util.*;
 
 public class Solution {
+    /*
+        time exceeded
+     */
     public int minSwapsCouples(int[] row) {
-        Set<Candidate> set = new HashSet<>();
-        Queue<Candidate> q = new LinkedList<>();
+        Set<String> set = new HashSet<>();
+        Queue<int[]> q = new LinkedList<>();
+        Queue<Integer> distance = new LinkedList<>();
 
-        Candidate candidate = new Candidate(row, 0);
-        set.add(candidate);
-        q.add(candidate);
+        set.add(Arrays.toString(row));
+        q.add(row);
+        distance.add(0);
 
         while(!q.isEmpty()) {
-            Candidate curCandidate = q.poll();
-            //System.out.println(Arrays.toString(curCandidate.row));
+            int curDistance = distance.poll();
+            int[] curRow = q.poll();
 
-            if(isAllCoupleHoldingHands(curCandidate.row))
-                return curCandidate.distance;
+            if(isAllCoupleHoldingHands(curRow))
+                return curDistance;
 
-            for(int i = 0 ; i < row.length - 1 ; ++i) {
-                for(int j = i + 1 ; j < row.length ; ++j) {
-                    int[] newCandidate = curCandidate.row.clone();
-                    int temp = newCandidate[i];
-                    newCandidate[i] = newCandidate[j];
-                    newCandidate[j] = temp;
+            for(int i = 0 ; i < row.length ; ++i) {
+                int[] swapped = swap(curRow.clone(), i);
+                if(swapped.length == 0)
+                    continue;
 
-                    Candidate newCandi = new Candidate(newCandidate, curCandidate.distance + 1);
+                String swappedStr = Arrays.toString(swapped);
 
-                    //System.out.println(Arrays.toString(curCandidate.row) + " -> " + Arrays.toString(newCandidate));
-
-                    if(!set.contains(newCandi)) {
-                        set.add(newCandi);
-                        q.add(newCandi);
-                    }
+                if(!set.contains(swappedStr)) {
+                    set.add(swappedStr);
+                    q.add(swapped);
+                    distance.add(curDistance + 1);
                 }
             }
         }
@@ -41,36 +41,35 @@ public class Solution {
         return -1;
     }
 
-    public boolean isAllCoupleHoldingHands(int[] row) {
-        for(int i = 0 ; i < row.length ; i += 2) {
-            if(row[i] % 2 == 0) {
-                if(row[i + 1] != row[i] + 1)
-                    return false;
-            } else {
-                if(row[i + 1] != row[i] - 1)
-                    return false;
+    private int[] swap(int[] arr, int i) {
+        int targetNum = arr[i];
+        int swapTargetIdx = (i % 2 == 0) ? i + 1 : i - 1;
+        int coupleNum = (targetNum % 2 == 0) ? targetNum + 1 : targetNum - 1;
+
+        int swapCoupleTargetIdx = 0;
+        for(int j = 0 ; j < arr.length ; ++j) {
+            if(arr[j] == coupleNum) {
+                swapCoupleTargetIdx = j;
+                break;
             }
+        }
+
+        if(Math.abs(i - swapCoupleTargetIdx) == 1)
+            return new int[] {};
+
+        int temp = arr[swapTargetIdx];
+        arr[swapTargetIdx] = arr[swapCoupleTargetIdx];
+        arr[swapCoupleTargetIdx] = temp;
+
+        return arr;
+    }
+
+    private boolean isAllCoupleHoldingHands(int[] row) {
+        for(int i = 0 ; i < row.length ; i += 2) {
+            if(Math.abs(row[i + 1] - row[i]) == 1)
+                return false;
         }
 
         return true;
-    }
-
-    public class Candidate {
-        public int[] row;
-        public int distance;
-
-        public Candidate(int[] row, int distance) {
-            this.row = row;
-            this.distance = distance;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            for(int i = 0 ; i < this.row.length ; ++i) {
-                if(this.row[i] != ((Candidate)obj).row[i])
-                    return false;
-            }
-            return true;
-        }
     }
 }
