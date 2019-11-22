2adc88968cef44d7c28e6725b3790c1338b3bda6
nokchax <nokchax@gmail.com>
2019-11-22 16:50:03 +0900

    191122

---
+++ src/leetcode/Q00329/Solution.java
@@ -0,0 +1,18 @@
+package leetcode.Q00329;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int longestIncreasingPath(int[][] matrix) {
+        int longestPath = 0;
+        int[][] cache = new int[matrix.length][matrix[0].length];
+
+        for(int i = 0 ; i < matrix.length ; ++i)
+            Arrays.fill(cache[i], -1);
+
+
+
+
+        return 0;
+    }
+}
