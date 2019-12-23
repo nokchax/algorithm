42fb706ea3ff57ae84931bc875b45f08decb75f0
nokchax <nokchax@gmail.com>
2019-12-23 20:09:32 +0900

    191215

--- src/leetcode/Q00032/Solution.java
+++ src/leetcode/Q00032/Solution.java
@@ -4,7 +4,50 @@ import java.util.Stack;
 
 public class Solution {
     public int longestValidParentheses(String s) {
+        int max = 0;
+        Stack<String> numStack = new Stack<>();
+        Stack<Character> charStack = new Stack<>();
 
-        return 0;
+        for(char c : s.toCharArray()) {
+            if(c == '(') {
+                numStack.push("(");
+                charStack.push('(');
+                continue;
+            }
+
+            // c == ')'
+            if(charStack.isEmpty()) {
+                numStack.push(")");
+            } else { // stack.peek() == '(';
+                charStack.pop();
+                int temp = 2;
+
+                while(!numStack.isEmpty() && !numStack.peek().equals("(")) {
+                    int num = Integer.parseInt(numStack.pop());
+                    temp += num;
+                }
+
+                if(!numStack.isEmpty()) {
+                    numStack.pop();
+                }
+                numStack.push(String.valueOf(temp));
+            }
+        }
+
+        while(!numStack.isEmpty()) {
+            int temp = 0;
+
+            while(!numStack.isEmpty() && !numStack.peek().equals("(") && !numStack.peek().equals(")")) {
+                int num = Integer.parseInt(numStack.pop());
+                temp += num;
+            }
+
+            if(!numStack.isEmpty()) {
+                numStack.pop();
+            }
+            max = Math.max(max, temp);
+        }
+
+        return max;
     }
 }
