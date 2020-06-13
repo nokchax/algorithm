73b7828b93ac5b5d65f9bc1616aa9d8ea9b8711f
nokchax <nokchax@gmail.com>
2020-06-14 00:08:07 +0900

    200614

--- src/main/java/leetcode/Q00045/Solution.java
+++ src/main/java/leetcode/Q00045/Solution.java
@@ -1,8 +1,25 @@
 package leetcode.Q00045;
 
+import java.util.Arrays;
+
 public class Solution {
     public int jump(int[] nums) {
+        int[] cache = new int[nums.length];
+        Arrays.fill(cache, Integer.MAX_VALUE);
+        cache[0] = 0;
 
-        return -1;
+        for (int i = 0 ; i < nums.length ; ++i) {
+            int max = Math.min(i + nums[i], nums.length - 1);
+
+            for (int j = max ; j > i ; --j) {
+                if (cache[i] + 1 <= cache[j]) {
+                    cache[j] = cache[i] + 1;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return cache[nums.length - 1];
     }
 }
