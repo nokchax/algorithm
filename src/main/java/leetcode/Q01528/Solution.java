466d050f575f054a6a9535cf771fb7ab14aa0f84
nokchax <nokchax@gmail.com>
2020-08-10 23:06:12 +0900

    200810

---
+++ src/main/java/leetcode/Q01528/Solution.java
@@ -0,0 +1,13 @@
+package leetcode.Q01528;
+
+public class Solution {
+    public String restoreString(String s, int[] indices) {
+        char[] answer = new char[indices.length];
+
+        for (int i = 0 ; i < indices.length ; ++i) {
+            answer[indices[i]] = s.charAt(i);
+        }
+
+        return new String(answer);
+    }
+}
