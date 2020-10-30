868a515fae14a169096b6315fee16d69ba39b5eb
nokchax <nokchax@gmail.com>
2020-10-30 19:51:36 +0900

    q5

---
+++ src/main/java/test/t2/q5/Solution.java
@@ -0,0 +1,38 @@
+package test.t2.q5;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class Solution {
+    private static final DateTimeFormatter FORMAT = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss", Locale.KOREA);
+
+    public static void main(String[] args) {
+        String input = "2019/05/01 00:59:19\n" +
+                        "2019/06/01 01:35:20\n" +
+                        "2019/08/01 02:01:22\n" +
+                        "2019/08/01 02:01:23\n" +
+                        "2019/08/02 03:02:35\n" +
+                        "2019/10/03 04:05:40\n" +
+                        "2019/10/04 06:23:10\n" +
+                        "2019/10/10 08:23:20\n" +
+                        "2019/10/12 08:42:24\n" +
+                        "2019/10/23 08:43:26\n" +
+                        "2019/11/14 08:43:29\n" +
+                        "2019/11/01 10:19:02\n" +
+                        "2019/12/01 11:23:10";
+
+        System.out.println(Arrays.toString(parse(input)));
+    }
+
+    public static int[] parse(String inputs) {
+        int[] answer = new int[24];
+        Arrays.stream(inputs.split("\n"))
+                .map(x -> LocalDateTime.parse(x, FORMAT))
+                .mapToInt(time -> (time.getHour() + 9) % 24)
+                .forEach(x -> answer[x]++);
+
+        return answer;
+    }
+}
