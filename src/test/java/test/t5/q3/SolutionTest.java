75ca09bfe3b33bab8be227d681b3ad911f000721
nokchax <nokchax@gmail.com>
2020-12-03 23:11:04 +0900

    apply assertj

--- src/test/java/test/t5/q3/SolutionTest.java
+++ src/test/java/test/t5/q3/SolutionTest.java
@@ -2,7 +2,7 @@ package test.t5.q3;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SolutionTest {
 
@@ -10,9 +10,9 @@ class SolutionTest {
     public void test() {
         Solution s = new Solution();
 
-        System.out.println(s.solution(new int[]{7,3,7,3,1,3,4,1}));
-        System.out.println(s.solution(new int[]{2,1,1,3,2,1,1,3}));
-        System.out.println(s.solution(new int[]{7,5,2,7,2,7,4,7}));
+        assertThat(s.solution(new int[]{7, 3, 7, 3, 1, 3, 4, 1})).isEqualTo(5);
+        assertThat(s.solution(new int[]{2, 1, 1, 3, 2, 1, 1, 3})).isEqualTo(3);
+        assertThat(s.solution(new int[]{7, 5, 2, 7, 2, 7, 4, 7})).isEqualTo(6);
     }
 
 }
