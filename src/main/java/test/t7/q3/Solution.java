2413095fb151982d96accd9fc536b3b7a1f0ab0a
nokchax <nokchax@gmail.com>
2020-12-26 23:32:52 +0900

    recursive

---
+++ src/main/java/test/t7/q3/Solution.java
@@ -0,0 +1,38 @@
+package test.t7.q3;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Solution {
+    public String solution(String riddle) {
+        if (riddle.equals("?")) {
+            return "a";
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0 ; i < riddle.length() ; ++i) {
+            char curChar = riddle.charAt(i);
+
+            // 일반 문자면 그냥 그대로
+            if (curChar != '?') {
+                sb.append(curChar);
+                continue;
+            }
+
+            // ?일경우에는
+            sb.append(getRandomChar(sb.length() == 0 ? null : sb.charAt(sb.length() - 1), i + 1 == riddle.length() ? null : riddle.charAt(i + 1)));
+        }
+
+        return sb.toString();
+    }
+
+    private char getRandomChar(Character prev, Character next) {
+        int i = ThreadLocalRandom.current().nextInt(26);
+
+        if ((prev != null && prev - 'a' == i) || (next != null && next - 'a' == i)) {
+            return getRandomChar(prev, next);
+        }
+
+        return (char) (i + 'a');
+    }
+}
