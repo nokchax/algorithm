dc359498ac4afe169421c950e15a5bf29ca48919
Kwanghyun Kim <32362748+nokchax@users.noreply.github.com>
2021-05-27 23:21:05 +0900

    Create Solution.java

---
+++ src/main/java/leetcode/Q01281/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+        String numStr = String.valueOf(n);
+        int product = 1;
+        int sum = 0;
+        for (char c : numStr.toCharArray()) {
+            int x = c - '0';
+            sum += x;
+            product *= x;
+        }
+        
+        return product - sum;
+    }
+}
