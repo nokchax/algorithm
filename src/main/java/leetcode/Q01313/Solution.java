38a635a98c9fe13e285309e86f167fe4a0d54d18
Kwanghyun Kim <32362748+nokchax@users.noreply.github.com>
2021-05-28 23:22:41 +0900

    Create Solution.java

---
+++ src/main/java/leetcode/Q01313/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0 ; i < nums.length ; i += 2) {
+            int repeat = nums[i];
+            int num = nums[i + 1];
+            
+            for (int j = 0 ; j < repeat ; ++j) {
+                answer.add(num);
+            }
+        }
+        
+        int[] array = new int[answer.size()];
+        for (int i = 0; i < answer.size() ; i++) {
+            array[i] = answer.get(i);
+        }
+        return array;
+    }
+}
