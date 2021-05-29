44cb6276eb7191a00adba657ef86f566e5acae6e
Kwanghyun Kim <32362748+nokchax@users.noreply.github.com>
2021-05-29 23:26:19 +0900

    Create Solution.java

---
+++ src/main/java/leetcode/Q01678/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+        Queue<Character> q = new LinkedList<>();
+        
+        for (char c : command.toCharArray()) {
+            if (c == '(') {
+                q.clear();
+                q.add('(');
+            } else if (c == ')') {
+                if (q.size() == 1) {
+                    sb.append('o');
+                }
+                q.clear();
+            } else {
+                q.add(c);
+                sb.append(c);
+            }
+        }
+        
+        return sb.toString();
+    }
+}
