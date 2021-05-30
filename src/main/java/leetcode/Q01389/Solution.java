72173280277af522de53813d01ff51b4de4ae7a8
Kwanghyun Kim <32362748+nokchax@users.noreply.github.com>
2021-05-30 22:23:02 +0900

    Create Solution.java

---
+++ src/main/java/leetcode/Q01389/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> array = new LinkedList<>();
+        
+        for (int i = 0 ; i < nums.length ; ++i) {
+            int idx = index[i];
+            array.add(idx, nums[i]);
+        }
+        
+        int[] answer = new int[array.size()];
+        for (int i = 0 ; i < array.size() ; ++i) {
+            answer[i] = array.get(i);
+        }
+        
+        return answer;
+    }
+}
