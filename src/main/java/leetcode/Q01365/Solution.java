a5fb052b8ee0bef487e9ad895602c4186663fdbf
Kwanghyun Kim <32362748+nokchax@users.noreply.github.com>
2021-05-31 23:45:30 +0900

    Create Solution.java

---
+++ src/main/java/leetcode/Q01365/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] stack = new int[101];
+        
+        for (int i = 0 ; i < nums.length ; ++i) {
+            stack[nums[i]]++;
+        }
+        
+        for (int i = 1 ; i < 101 ; ++i) {
+            stack[i] += stack[i - 1];
+        }
+        
+        int[] answer = new int[nums.length];
+        
+        for (int i = 0 ; i < nums.length ; ++i) {
+            int target =  nums[i] - 1;
+            answer[i] = target < 0 ?  0 : stack[target];
+        }
+        
+        return answer;
+    }
+}
